2056862464202a0bf4ccf6edfac9001e54e36a77
Nell-jay <gianganwneljae@gmail.com>
2026-08-03 22:09:06 +0800

    Added adsense js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-8d0d8005'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pvb24o0qfe8"
+    "revision": "0.v219088po48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
